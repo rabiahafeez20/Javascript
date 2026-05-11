78555e9c47d29e733d3d6c5e302734450583d727
rabiahafeez20 <rabiahafeez105567@gmail.com>
2026-05-10 23:49:12 -0800

    javascript

---
+++ Variable for Numbers/app.js
@@ -0,0 +1,25 @@
+
+// declare age
+
+var age = 15;
+alert(" I am " + age + " years old")
+
+
+// declare visits
+ var visits = 14;
+document.write(" You have visited this site " + visits + " times <br>");
+
+
+// declare birth year
+
+var birthYear = 1990;
+document.write("My birth year is " + birthYear + "<br>");
+document.write("Data type of my declared variable is " + typeof(birthYear) + "<br>");
+
+// declare visits on clothing site
+
+var visitorName = "John Doe";
+var productTitle = "T-shirt";
+var quantity = 5;
+
+document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store");
---
+++ Variables for Strings/app.js
@@ -0,0 +1,45 @@
+
+// declare username
+var username;
+
+// declare myname
+var myName = "Rabiya Hafeez";
+
+
+
+// declare message
+
+var message = "Hello World";
+alert(message);
+
+
+//  student’s bio data
+
+var studentName = "Jhony Doe";
+var studentAge = "15 years old";
+var studentCourse = "Certified Mobile Application Development";
+
+alert(studentName);
+alert(studentAge);
+alert(studentCourse);
+
+
+var pizzaMessage = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizzaMessage);
+
+
+var email = "example@example.com";
+alert("My email address is " + email);
+
+
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+
+
+document.write("Yah! I can write HTML content through JavaScript");
+
+
+
+var specialString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(specialString);
+document.write("<br>" + specialString);
